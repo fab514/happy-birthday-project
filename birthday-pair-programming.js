41e1260a00b095c7fc2f462b9220f6d6791c5177
Daniel <dan@dgr.dev>
2021-02-23 23:58:29 -0600

    feat(birthday-pair-programming): add functionality to createHalfBirthdayTable

--- birthday-pair-programming.js
+++ birthday-pair-programming.js
@@ -1,90 +1,86 @@
 // GOAL: To display a table in the console of people's half birthdays.
 // We are going to sort this file by variables, functions, and execution.
 
-
 // VARIABLES
 const gameGroup = [
-    {
-        birthday: 'April 16, 1991',
-        name: {
-            first: 'Anna',
-            last: 'Brekke'
-        },
+  {
+    birthday: "April 16, 1991",
+    name: {
+      first: "Anna",
+      last: "Brekke",
     },
-    {
-        birthday: 'February 27, 1990',
-        name: {
-            first: 'Andrew',
-            last: 'Brenner'
-        },
+  },
+  {
+    birthday: "February 27, 1990",
+    name: {
+      first: "Andrew",
+      last: "Brenner",
     },
-    {
-        birthday: 'July 22, 1989',
-        name: {
-            first: 'Felicia',
-            last: 'Brenner'
-        },
+  },
+  {
+    birthday: "July 22, 1989",
+    name: {
+      first: "Felicia",
+      last: "Brenner",
     },
-    {
-        birthday: 'December 8, 1987',
-        name: {
-            first: 'Dan',
-            last: 'Richmond'
-        },
+  },
+  {
+    birthday: "December 8, 1987",
+    name: {
+      first: "Dan",
+      last: "Richmond",
     },
-    {
-        birthday: 'January 30, 1989',
-        name: {
-            first: 'Justin',
-            last: 'Veres'
-        },
+  },
+  {
+    birthday: "January 30, 1989",
+    name: {
+      first: "Justin",
+      last: "Veres",
     },
-]
-
-const birthdays 
-
-
-
-
-
-
+  },
+];
 
 // FUNCTIONS
-function calculateHalfBirthday(birthday) {
-    const [month, date, year] = new Date(birthday);
-    // The birthday should account for both month and day.
-    // Take the birthday that's coming in,
-    // Find the day that is six months away from their full birthday.
-    console.log('month', month)
-    console.log('date', date)
-    console.log('year', year)
-    return'calculation goes here';
-}
-
 function createHalfBirthdayTable(groupMembers) {
-    // groupMembers is the gameGroup variable declared above.
-    // We want to take that table data, and replace birthdays with half-birthdays.
-    // the array of group members is mapped to a new array
-    const newTable = groupMembers.map(groupMember => {
-        // each group member's half birthday is calculated with the calculateHalfBirthday function, and assigned to the halfBirthday key
-        groupMember.halfBirthday = calculateHalfBirthday(groupMember.birthday)
-        // each group member's name remains the same, so we assign it to itself, preserving it in the new array
-        groupMember.name = groupMember.name;
-        // we return each group member in the map so we don't end up with an array of undefined items
-        return groupMember;
-    })
-    // The original array should not be mutated
-    // return that new table data
-    return newTable
+  return groupMembers.map((member) => {
+    // Create a new object so we aren't mutating key-value pairs in the original object.
+    // I used let because we are modifying this object, and even though const lets us do this, the let gives clarity to the fact that we are modifying parts of the object.
+    let halfBirthdayObject = {};
+    // set the first name to be the first name from the original object.
+    // This is at the top level because console.table will format more nicely this way.
+    halfBirthdayObject.firstName = member.name.first;
+    // set the last name to be the last name from the original object.
+    halfBirthdayObject.lastName = member.name.last;
+    // Create a new Date object from the birthday so we have access to methods on the Date constructor.
+    const halfBirthday = new Date(member.birthday);
+    // getMonth starts with 0 for January, up to 11 for December, so we are checking this against 11 instead of 12.
+    // if halfBirthday.getMonth returns greater than 11, it is in the second half of the year
+    halfBirthday.getMonth() + 6 > 11
+      ? // if in the second half of the year, we subtract 6
+        halfBirthday.setMonth(halfBirthday.getMonth() - 6)
+      : // otherwise we add 6
+        halfBirthday.setMonth(halfBirthday.getMonth() + 6);
+    // Set the halfBirthday value to the halfBirthday
+    halfBirthdayObject.halfBirthday = halfBirthday
+      // as a date string
+      .toDateString()
+      // then split the string into an array at the spaces
+      .split(" ")
+      // then slice the year off of the array
+      .slice(0, -1)
+      // then join the array back into a string
+      .join(" ");
+    // Return the newly created object
+    return halfBirthdayObject;
+  });
 }
-
 
 // EXECUTION OF THE JAVASCRIPT
 // Initial table of the game group for debugging purposes.
 // I'm using the string so it's easy to track this among the other values we might be console.logging along the way.
 // Log out a table of people with their birthday
-console.table('gameGroup', gameGroup);
+console.table(gameGroup);
 // Take that array of people
 // For each person, we want to calculate their half-birthday
 // Log out a new table of people with their half-birthday
-console.table('halfBirthdayTable', createHalfBirthdayTable(gameGroup));
+console.table(createHalfBirthdayTable(gameGroup));
